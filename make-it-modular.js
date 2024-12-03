5754fdf889b9c42b930326df976bddc40c1deeae
Petbrawl <jh0693530@gmail.com>
2024-12-03 12:45:26 -0500

    Cambios

--- make-it-modular.js
+++ make-it-modular.js
@@ -1,9 +1,15 @@
-const mymodule = require
+const mymodule = require('./mymodule');
 
 const dirname = process.argv[2];
 const ext = process.argv[3];
 
-mymodule(dirname, ext, function(err, data){
-    if(err) console.log(err);
-    console.log(data);
-})
+mymodule(dirname, ext, function (err, data) {
+    if (err) {
+        console.error(err);
+        return;
+    }
+
+    data.forEach(function(file) {
+        console.log(file);
+    });
+});
--- mymodule.js
+++ mymodule.js
@@ -1,16 +1,13 @@
-const fs = require("fs");
+const fs = require('fs');
+const path = require('path');
 
-module.exports = function (dirname,ext,callback) {
+module.exports = function (dirname, ext, callback) {
     fs.readdir(dirname, function (err, list) {
         if (err) {
-            return callback(err);
+            return callback(err);  
         }
 
-        const result = [];
-        list.forEach(function (file){
-            file.split(".")[1] === ext ? result.push(file) : null;
-        })
-
-        callback(null, result);
-    })
-}
+        const result = list.filter(file => path.extname(file) === `.${ext}`);
+        callback(null, result); 
+    });
+};
